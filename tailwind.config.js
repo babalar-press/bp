c34ea4969c63b8aff22ad3eac0dd90e6ddb1ffad
babalar-press <babalarpress@gmail.com>
2024-01-29 18:12:45 +0100

    tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,20 @@ const typography = require('@tailwindcss/typography');
 module.exports = {
 	content: ['./hugo_stats.json'],
 	plugins: [typography],
-		colors: {
-			'lemon-yellow': '#f1ff78',
-			'lilac-grey': '#e9e6ed',
-			'white': '#ffffff',
-			'black': '#000000',
-			'neon-green': '#00ff00',
-		},
+	theme: {
+		extend: {
+			colors: {
+				'lemon-yellow': '#f1ff78',
+     			'lilac-grey': '#e9e6ed',
+        		'white': '#ffffff',
+        		'black': '#000000',
+        		'neon-green': '#00ff00',
+			},
+			boxShadow: {
+				'2xl': '10px 8px 8px 20px',
+			}
+		}
+	}
 };
+
+
